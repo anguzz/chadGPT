ad69410521b95cff2da379f0c19e70c0eaee7d1e
Oscar Cisneros <41174627+oscisn93@users.noreply.github.com>
2023-04-09 07:26:29 +0000

    pushing to master

---
+++ src/components/Message.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { ChatCompletionRequestMessageRoleEnum } from 'openai';
+	export let role_type: ChatCompletionRequestMessageRoleEnum;
+	export let message: string;
+</script>
+
+<div>
+	{'message goes here'}
+</div>
